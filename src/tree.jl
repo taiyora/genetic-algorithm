2269c282855c49ebb6fd064217ffe0a1565b90e5
taiyora <taiyora@protonmail.com>
2018-02-16 02:57:35 +1300

    Implement random Tree generation and evaluation

--- src/tree.jl
+++ src/tree.jl
@@ -6,26 +6,42 @@ For example, a node with the '+' operator and three children with the values 1, 
 will return the value 16 when evaluated.
 
 Of course, any child may also be an operator; in this way, complex expressions can be built.
+Values are of type Float64, and randomly generated values are between 0.0 and 1.0
 """
 module TreeNode
 	"A node can function as either a value, or an operator on its children (e.g. `+`)."
-	Function = Union{Int, Char}
+	Function = Union{Float64, Char}
+
+	operators = ['+', '-', '*']
 
 	type Node
 		valOp::Function # The value or operator that the node functionally represents
 		children::Vector{Node}
 	end
 
-	Node()                = Node(0,     Vector{Node}())
+	Node()                = Node(0.0,   Vector{Node}())
 	Node(valOp::Function) = Node(valOp, Vector{Node}())
 
-	function setValue(node::Node, value::Int)
+	function generateRandom()::Node
+		if rand(Bool)
+			# Generate a Value node
+			value = rand()
+			return Node(value)
+
+		else
+			# Generate an Operator node
+			index = rand(1:length(operators))
+			return Node(operators[index])
+		end
+	end
+
+	function setValue(node::Node, value::Float64)
 		@assert isLeaf(node) # Only a leaf can be a value; a parent must be an operator
 		node.valOp = value
 	end
 
 	function setOperator(node::Node, operator::Char)
-		@assert operator in ['+', '-', '*']
+		@assert operator in operators
 		node.valOp = operator
 	end
 
@@ -51,18 +67,100 @@ module Tree
 	using TreeNode
 
 	"Given a root node (or any node in fact), will print the tree as a nested expression."
-	function printTree(node::TreeNode.Node)
-		if typeof(node.valOp) == Int
-			print(node.valOp, ' ')
+	function printAsExpr(node::TreeNode.Node)
+		if typeof(node.valOp) == Float64
+			print(' ', node.valOp)
 		
 		else
-			print('(', node.valOp, ' ')
+			print(" (", node.valOp)
 
 			for child in node.children
-				printTree(child)
+				printAsExpr(child)
 			end
 
 			print(')')
+		end
+	end
+
+	"Returns the number of nodes in a given tree."
+	function getSize(node::TreeNode.Node)::Int
+		size = 1
+
+		for child in node.children
+			size += getSize(child)
+		end
+
+		return size
+	end
+
+	"Returns a tree as a list of its nodes."
+	function toVector(node::TreeNode.Node)::Vector{TreeNode.Node}
+		nodes = [node]
+
+		for child in node.children
+			nodes = vcat(nodes, toVector(child))
+		end
+
+		return nodes
+	end
+
+	"Returns a random node from a given tree."
+	function getRandomNode(node::TreeNode.Node)::TreeNode.Node
+		nodeVector = toVector(node)
+		nNodes = length(nodeVector)
+
+		index = rand(1:nNodes)
+		return nodeVector[index]
+	end
+
+	"Generates a completely random tree with the given number of nodes."
+	function generateRandom(nNodes::Int)::TreeNode.Node
+		root = TreeNode.generateRandom()
+
+		# Continously find random nodes to make into parents
+		for i in 2:nNodes # Start at 2, since we begin with a root node
+			target = getRandomNode(root)
+
+			if typeof(target.valOp) == Float64
+				index = rand(1:length(TreeNode.operators))
+				TreeNode.setOperator(target, TreeNode.operators[index])
+			end
+
+			TreeNode.addChild(target, TreeNode.generateRandom())
+		end
+
+		return fix(root)
+	end
+
+	"Finds Operator nodes with no children and turns them into Value nodes."
+	function fix(node::TreeNode.Node)::TreeNode.Node
+		if length(node.children) == 0 && typeof(node.valOp) == Char
+			node.valOp = rand()
+		
+		else
+			for child in node.children
+				child = fix(child)
+			end
+		end
+
+		return node
+	end
+
+	"Evaluates a tree as an expression, resulting in a single output."
+	function evaluate(node::TreeNode.Node)::Float64
+		if typeof(node.valOp) == Char
+			if node.valOp == '+'
+				return reduce(+, map(evaluate, node.children))
+
+			elseif node.valOp == '-'
+				return reduce(-, map(evaluate, node.children))
+
+			else # multiplication
+				return reduce(*, map(evaluate, node.children))
+			end
+
+		else
+			return node.valOp
 		end
 	end
 end
